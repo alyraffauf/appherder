4ba11af383d6dcc0d4e3354e14d4fc0b8cf22da5
Aly Raffauf <aly@aly.codes>
2026-06-20 10:18:37 -0400

    ci: build appimages with nix

--- package.nix
+++ package.nix
@@ -3,18 +3,22 @@
   dwarfs,
   lib,
   makeWrapper,
-}: let
-  version = "dev";
-in
-  buildGoModule {
-    pname = "appherder";
-    inherit version;
-    src = ./.;
-    vendorHash = "sha256-hrqRutMJw96V61AKoyt4Oqya8STeGYlF5phb6O35L8Q=";
-    subPackages = ["cmd/appherder"];
-    ldflags = ["-X main.version=${version}"];
-    nativeBuildInputs = [makeWrapper];
-    postInstall = ''
-      wrapProgram $out/bin/appherder --prefix PATH : ${lib.makeBinPath [dwarfs]}
-    '';
-  }
+  version ? "dev",
+}:
+buildGoModule {
+  pname = "appherder";
+  inherit version;
+  src = ./.;
+  vendorHash = "sha256-hrqRutMJw96V61AKoyt4Oqya8STeGYlF5phb6O35L8Q=";
+  subPackages = ["cmd/appherder"];
+  env.CGO_ENABLED = "0";
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${version}"
+  ];
+  nativeBuildInputs = [makeWrapper];
+  postInstall = ''
+    wrapProgram $out/bin/appherder --prefix PATH : ${lib.makeBinPath [dwarfs]}
+  '';
+}
